cc11be3e43a0d5a5433b6e1e1c47522b15491d1e
luqiwee <277180166@qq.com>
2023-09-27 01:19:58 +0800

    add

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+use syn::DeriveInput;
+
+#[proc_macro_derive(Build)]
+pub fn build_macrp_derive(input: TokenStream) -> TokenStream {
+    // 基于 input 构建 AST 语法树
+    let ast: DeriveInput = syn::parse(input).unwrap();
+
+    // 构建特征实现代码
+    impl_hello_macro(&ast)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
 }
